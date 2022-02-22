acbc9d870e14068e684ef05bc60b83b8832e3527
pattytonyoneill <pattytonyoneill@gmail.com>
2022-02-22 13:16:09 +0000

    Code-Along Challenge

--- student.py
+++ student.py
@@ -18,6 +18,9 @@ class Student:
     @property
     def email(self):
         return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
-   
+
     def alert_santa(self):
         self.naughty_list = True
+
+    def apply_extension(self, days):
+        self.end_date += timedelta(days=days)
